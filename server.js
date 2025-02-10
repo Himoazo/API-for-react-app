f9b334ff431c0ea9b8b6a13f6c356fb8612e910f
Hoomam <hoaz1600@student.miun.se>
2025-02-10 03:00:17 +0100

    Production deployment script

--- server.js
+++ server.js
@@ -9,18 +9,17 @@ app.use(cors());
 
 
 // Middleware to validate input
-function inputValidation(req, res, next) {
-    
-    const { todo, description, status } = req.body;
+function inputValidation(req, res, next) {    
+    const { todo_name, description, status } = req.body;
 
-    if (typeof todo !== "string" || (description && typeof description !== "string")) {
+    if (typeof todo_name !== "string" || (description && typeof description !== "string")) {
         return res.status(400).json({ error: "Todo and description must be strings" });
     }
 
-    if (!todo || todo.trim() === "") {
+    if (!todo_name || todo_name.trim() === "") {
         return res.status(400).json({ error: "Name can't be empty" });
     }
-    if (todo.length < 3 || todo.length > 30) {
+    if (todo_name.length < 3 || todo_name.length > 50) {
         return res.status(400).json({ error: "Name must be between 3 and 30 chars" });
     }
 
@@ -56,13 +55,15 @@ app.get('/api/:id', (req, res) => {
 });
 
 app.post('/api', inputValidation, (req, res) => {
-    const { todo, description, status } = req.body;
-
+    const { todo_name, description, status } = req.body;
     
     const insertion = db.prepare("INSERT INTO Todos (todo_name, description, status) VALUES (?, ?, ?)");
-    insertion.run(todo, description, status, (err) => {
+    insertion.run(todo_name, description, status, (err) => {
         if (err) {
-            return res.status(500).json({ error: err.message })
+            if (err.code === "SQLITE_CONSTRAINT") {
+                return res.status(422).json({ error: "Todo name must be unique" });
+            }
+            return res.status(500).json({ error: err })
         }
         res.status(201).json({ succes: "A todo has been created" });
         insertion.finalize();
@@ -72,7 +73,7 @@ app.post('/api', inputValidation, (req, res) => {
   
 
 app.put('/api/:id', inputValidation, (req, res) => {
-    const { todo, description, status } = req.body;
+    const { todo_name, description, status } = req.body;
     const { id } = req.params;
 
     db.get("SELECT id FROM Todos WHERE id = ?", [id], (err, row) => {
@@ -80,7 +81,7 @@ app.put('/api/:id', inputValidation, (req, res) => {
         if (!row) return res.status(404).json({ error: "Todo was not found" });
 
         const update = db.prepare("UPDATE Todos SET todo_name = ?, description = ?, status = ? WHERE id = ?");
-        update.run(todo, description, status, id, function (err) {
+        update.run(todo_name, description, status, id, function (err) {
             if (err) return res.status(500).json({ error: err.message });
 
             res.status(200).json({ success: "A todo has been updated" });
